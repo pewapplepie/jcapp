a60da5b80c8f43a6022c43c6740a940b8d773507
Jer-Shen Chen <jschen1217@gmail.com>
2024-06-21 19:01:33 -0400

    add aboutme

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import { useEffect } from 'react';
+import AboutMeContent from '../components/AboutMeContent';
 
 const About = () => {
+  useEffect(() => {
+    const scrollAnimElements = document.querySelectorAll('[data-animate-on-scroll]');
+    const observer = new IntersectionObserver(
+      (entries) => {
+        for (const entry of entries) {
+          if (entry.isIntersecting || entry.intersectionRatio > 0) {
+            const targetElement = entry.target;
+            targetElement.classList.add('animate');
+            observer.unobserve(targetElement);
+          }
+        }
+      },
+      {
+        threshold: 0.15,
+      }
+    );
+
+    scrollAnimElements.forEach(element => observer.observe(element));
+
+    return () => {
+      scrollAnimElements.forEach(element => observer.unobserve(element));
+    };
+  }, []);
+
   return (
-    <div className="p-4">
-      <h2 className="text-xl font-bold">About Page</h2>
-      <p>This is the about page.</p>
+    <div className="w-full relative bg-gray-100 overflow-hidden flex flex-col items-start justify-start box-border">
+      <main className="flex-1 overflow-hidden flex flex-col items-start justify-center pt-9 pb-32 pr-24 pl-24 box-border gap-20 max-w-full">
+        <AboutMeContent />
+      </main>
     </div>
   );
 };
